c33494361bd7433d7e231797914e16e69495d4ab
carsonpo <38706134+carsonpo@users.noreply.github.com>
2018-12-12 20:57:00 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 wget https://storage.googleapis.com/mltechnicalanalysis/train_data.zip
 unzip train_data.zip -d train_data
-pip3 install torch
-pip3 install torchvision
-pip3 install dominate
-pip3 install visdom
-python3 !python3 train.py --dataroot ./train_data --name twilight_cyclegan --model cycle_gan --resize_or_crop scale_width_and_crop --loadSize 1024 --fineSize 360
+pip install torch
+pip install torchvision
+pip install dominate
+pip install visdom
+python3 train.py --dataroot ./train_data --name twilight_cyclegan --model cycle_gan --resize_or_crop scale_width_and_crop --loadSize 1024 --fineSize 360
 python3 -m visdom.server
 
