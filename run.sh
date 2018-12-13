8546ced5a2bfa892b9acabf117b39cec5fa7ff28
carsonpo <38706134+carsonpo@users.noreply.github.com>
2018-12-12 21:24:31 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -7,6 +7,6 @@ pip install torch
 pip install torchvision
 pip install dominate
 pip install visdom
-python train.py --dataroot ./train_data/train_data1 --name twilight_cyclegan --model cycle_gan --resize_or_crop scale_width_and_crop --loadSize 1024 --fineSize 360
+python train.py --dataroot ./train_data/train_data1 --name twilight_cyclegan --model cycle_gan --resize_or_crop scale_width_and_crop --loadSize 1024 --fineSize 360 --display_id 0
 python -m visdom.server
 
