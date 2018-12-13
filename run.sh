2b417848002b09668e5a1f3d814211aba5a655b0
carsonpo <38706134+carsonpo@users.noreply.github.com>
2018-12-13 15:13:00 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,13 +1,9 @@
 #!/usr/bin/env bash
-apt-get update
-apt-get install wget
-wget https://storage.googleapis.com/mltechnicalanalysis/train_data.zip
-unzip train_data.zip -d train_data
-mkdir checkpoints
+unzip /storage/train_data/train_data.zip -d train_data
 pip install torch
 pip install torchvision
 pip install dominate
 pip install visdom
-python train.py --dataroot ./train_data/train_data1 --name twilight_cyclegan --model cycle_gan --resize_or_crop scale_width_and_crop --loadSize 1024 --fineSize 360 --display_id 0
-zip -r  /artifacts/checkpoints.zip ./checkpoints
+python train.py --dataroot ./train_data/train_data1 --name twilight_cyclegan --model cycle_gan --resize_or_crop scale_width_and_crop --loadSize 1024 --fineSize 360 --display_id 0 --checkpoints_dir /storage/checkpoints
+zip -r  /artifacts/checkpoints.zip /storage/checkpoints
 
