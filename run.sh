3ccf4a845baa13023e0feaf2dd87bff1bd256b30
carsonpo <38706134+carsonpo@users.noreply.github.com>
2018-12-14 00:17:04 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
-unzip /storage/train_data/train_data.zip -d train_data
 pip install torch
 pip install torchvision
 pip install dominate
 pip install visdom
-python train.py --dataroot ./train_data/train_data1 --name twilight_cyclegan --model cycle_gan --resize_or_crop scale_width_and_crop --loadSize 1024 --fineSize 360 --display_id 0 --checkpoints_dir /storage/checkpoints
+python train.py --dataroot /storage/flambient_data --name flambient_cyclegan --model cycle_gan --resize_or_crop scale_width_and_crop --loadSize 1024 --fineSize 360 --display_id 0 --checkpoints_dir /storage/flambient_chk
 zip -r  /artifacts/checkpoints.zip /storage/checkpoints
 
